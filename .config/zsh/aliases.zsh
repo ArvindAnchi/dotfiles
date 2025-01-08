5ee21f46bee9a54b06afcc6a807e2275ee49b6c5
Arvind Anchi <arvindanchi@gmail.com>
2025-01-08 09:52:16 -0400

    2025-01-08T13:52:16+00:00

---
+++ .config/zsh/aliases.zsh
@@ -0,0 +1,27 @@
+alias ls='ls --color=auto'
+alias l='ls -l'
+alias ll='ls -lahF'
+alias la='ls -alh'
+
+alias cp='cp -iv'
+alias mv='mv -iv'
+alias rm='rm -iv'
+
+alias grep="grep -P -i --color=auto"
+
+alias vim='nvim'
+alias vi='nvim'
+
+alias gl='git pull'
+alias gp='git push'
+alias gpf='git push --force-with-lease'
+
+alias gst='git status -s'
+alias gbr='git branch --remote'
+alias gd='git diff'
+
+alias glo='git log --oneline --decorate'
+alias gloga='git log --oneline --decorate --graph --all'
+alias glod='git log --graph --pretty="%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset"'
+alias gloga='git log --oneline --decorate --graph --all'
+alias glg='git log --stat'
