ecd62444841fff304c3b3f2b66c40cfc6f0e55ad
Arvind <arvind.anchi@gmail.com>
2025-02-02 21:39:04 +0000

    2025-02-02T21:39:04+00:00

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -1,7 +1,5 @@
 alias ls='ls --color=auto'
-alias l='ls -l'
-alias ll='ls -lahF'
-alias la='ls -alh'
+alias la='ls -lahF'
 
 alias cp='cp -iv'
 alias mv='mv -iv'
@@ -14,21 +12,24 @@ alias grep="grep -P -i --color=auto"
 alias vim='nvim'
 alias vi='nvim'
 
+alias gst='git status -s'
+
 alias gf='git fetch'
+alias grb='git rebase'
 alias gl='git pull'
 alias gp='git push'
 alias gpf='git push --force-with-lease'
 
-alias gst='git status -s'
 alias gstl='git stash list'
 alias gsta='git stash push'
 alias gstp='git stash pop'
+alias gstd='git stash drop'
+
 alias gbr='git branch --remote'
-alias gd='git diff'
+alias gsw='git switch'
 alias gsh='git show'
+alias gd='git diff'
 
 alias glo='git log --oneline --decorate'
-alias gloga='git log --oneline --decorate --graph --all'
-alias glod='git log --graph --pretty="%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset"'
-alias gloga='git log --oneline --decorate --graph --all'
 alias glg='git log --stat'
+alias glod='git log --graph --pretty="%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%ad) %C(bold blue)<%an>%Creset" --all'
