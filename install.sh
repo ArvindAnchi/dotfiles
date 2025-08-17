818aa364da050be38b349ec5803e55413327a0f0
Arvind <arvindanchi@gmail.com>
2025-08-17 20:38:38 +0400

    2025-08-17T16:38:38+00:00

--- install.sh
+++ install.sh
@@ -1,19 +1,13 @@
 #!/bin/bash
 
+rm -rf ~/.config/nvim/*
+rm -rf ~/.config/htop/
+rm -rf ~/.config/nvim/
+rm -rf ~/.config/pip/
+rm -rf ~/.config/tmux/
+rm -rf ~/.config/zsh/
+
 cp ~/dotfiles/.zshenv ~/.zshenv
 
 cp -r ~/dotfiles/.config/* ~/.config
 cp -r ~/dotfiles/.local/* ~/.local
-
-if [ ! -d ~/.virtualenvs ]; then
-    echo "Create virtualenv for nvim"
-
-    mkdir ~/.virtualenvs
-    python3 -m venv ~/.virtualenvs/neovim
-    source ~/.virtualenvs/neovim/bin/activate
-    pip install jupytext pynvim jupyter_client cairosvg plotly kaleido pnglatex pyperclip
-    deactivate
-
-    echo "Install libmagickwand-dev and quarto (https://github.com/quarto-dev/quarto-cli/releases/)"
-fi
-
