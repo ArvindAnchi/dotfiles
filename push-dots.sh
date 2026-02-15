3832d9226e6440f140f56fef345b65b0bbdcdc43
Arvind Anchi <arvindanchi@gmail.com>
2026-02-15 18:36:09 +0400

    2026-02-15T14:36:09+00:00

--- push-dots.sh
+++ push-dots.sh
@@ -1,16 +1,5 @@
 #!/bin/bash
 
-cd ~/dotfiles
-
-mkdir -p ~/dotfiles/.config
-
-cp ~/.zshenv ~/dotfiles
-
-cp -r ~/.config/nvim ~/dotfiles/.config
-cp -r ~/.config/htop ~/dotfiles/.config
-cp -r ~/.config/zsh ~/dotfiles/.config
-cp -r ~/.config/tmux ~/dotfiles/.config
-
 git add --all
 git commit -m "$(date -uIseconds)"
 git push
